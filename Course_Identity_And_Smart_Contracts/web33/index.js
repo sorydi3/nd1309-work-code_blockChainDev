172c5d50bda6adf06dd020cfba2e4a9a3a078c4a
ibradi3 <ibrahimasory22@hotmail.com>
2022-06-22 02:08:23 +0200

    on it

--- Course_Identity_And_Smart_Contracts/web33/index.js
+++ Course_Identity_And_Smart_Contracts/web33/index.js
@@ -1,42 +1,66 @@
 let web3 = require('web3');
+let Ethereuntx = require('@ethereumjs/tx');
+const common = require('ethereumjs-common').Common
 //let Contract = require('web3-eth-Contract');
 
 let url = 'http://127.0.0.1:8545'; // ganache local blockchain url
-let address = '0x6686f16bC4789A358C99E382C356413aE6a44A1f'; //Ganache address
-
+let address = '0x6686f16bC4789A358C99E382C356413aE6a44A1f'; //Ganache address //SENDING ADDRESS
+let ReceavingAddress = "0xcb034C02f7fd6C7fbA443c52FA6C5Ec48682A7B2";
+let PK = '688db2e3ac3ba6a2a6186490421f26b5d39ca22dd2fe64854b9f742630adf9bd'; //Private key of the sender
+let PKHex = new Buffer(PK,'hex');
 let WEB3 = new web3(url);
 
+
+//CHECK THE BALANCE OF EACH OF THE ADDRESSES
 WEB3.eth.getBalance(address,(err,bal) => {balance  = bal;}).then((bal_) =>{
     let numEth = WEB3.utils.fromWei(bal_)
-    console.log(`Your balance is ${numEth}`);
+    console.log(`SENDER BALANCE IS:  ${numEth}`);
 });
 
+// DISPLAY HERE THE BALANCE OF BOTH ACCOUNTS , THE SENDER AND THE RECEAVER
+WEB3.eth.getBalance(address,(err,bal) => {balance  = bal;}).then((bal_) =>{
+    let numEth = WEB3.utils.fromWei(bal_)
+    console.log(`RECEAVER BALANCE IS: ${numEth}`);
+});
 
-WEB3.eth.getTransactionCount(address).then((count) =>{
+WEB3.eth.getTransactionCount(ReceavingAddress).then((count) =>{
     console.log(`THE TRANSACTION ACOUNT OF THE EOA IS THE FOLLOWING NUMBER: ${count}`);
 });
 
-
+// HERE WE CREATE A TRANSACTION
 
 WEB3.eth.getAccounts().then((count) =>{
     console.log(`DISPLAY THE NUMBER AVAILABLE IN THE CHAIN: ${count}`);
 });
 
+const networkId =   WEB3.eth.net.getId();
+const chainId =  WEB3.eth.getChainId();
 
-// READING CONTRACT DATA (CA)
+const customCommon = common.forCustomChain(
+    'mainnet',
+    {
+      name: 'my-network',
+      networkId: networkId,
+      chainId: chainId,
+    },
+    'petersburg',
+  );
 
-/* let CAddress = '0x0D8775F648430679A709E98d2b0Cb6250d2887EF';
-let ABI = [{ "constant": true, "inputs": [], "name": "batFundDeposit", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "batFund", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "tokenExchangeRate", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "finalize", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "version", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "refund", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "tokenCreationCap", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "isFinalized", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "fundingEndBlock", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "ethFundDeposit", "outputs": [{ "name": "", "type": "address" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "createTokens", "outputs": [], "payable": true, "type": "function" }, { "constant": true, "inputs": [], "name": "tokenCreationMin", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "fundingStartBlock", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "remaining", "type": "uint256" }], "payable": false, "type": "function" }, { "inputs": [{ "name": "_ethFundDeposit", "type": "address" }, { "name": "_batFundDeposit", "type": "address" }, { "name": "_fundingStartBlock", "type": "uint256" }, { "name": "_fundingEndBlock", "type": "uint256" }], "payable": false, "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" }], "name": "LogRefund", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" }], "name": "CreateBAT", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "_from", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "_owner", "type": "address" }, { "indexed": true, "name": "_spender", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" }], "name": "Approval", "type": "event" }];
+let rawTransaction = {
+    nonce : 0,
+    to : ReceavingAddress,
+    gasPrice : 2000000,
+    gasLimit : 30000,
+    value :1,
+    data : ""
+}
 
-let contract = new WEB3.eth.Contract(ABI, CAddress);
-contract.methods.name().call((err, name) => {
-    console.log(`THE NAME OF THE CONTRACT IS : ${name}`);
-});
+console.log(`TRANSACTION BEING CARRIED OUT IS THE FOLLOWING: ${rawTransaction}`);
+ 
+var EtherTransaction = new Ethereuntx.Transaction(rawTransaction,{common: customCommon},); // CREATING A RAW TRANSACTION
+EtherTransaction.sign(PKHex); // SENDER SIGNING THE TRANSACTION
 
+//NOW WE SEND THE TRANSACTION TO THE NEWORK, IN OUR CASE IN THE LOCAL GANACHE NETWORK.
+let serializedTransaction = EtherTransaction.serialize();
+WEB3.eth.sendSignedTransaction(serializedTransaction);
 
-contract.methods.version().call((err, version) => {
-    console.log(`THE VERSION OF THE CONTRACT IS : ${version}`);
-}); */
-
-
-//GANACHE LOCAL BLOCKCHAIN
